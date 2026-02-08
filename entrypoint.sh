0071a2e647ab770e8e9f576b64c21df95921c2cc
yangDream-wang <52024450+yangDream-wang@users.noreply.github.com>
2026-02-08 20:42:36 +0800

    复制uptime-kuma到用户目录
    Replace ownership change with directory copy for uptime-kuma.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
 usermod -aG sudo $SSH_USER
 echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
 echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
-chown -R $SSH_USER:$SSH_USER /opt/uptime-kuma
+cp -r /tmp/uptime-kuma /home/$SSH_USER/uptime-kuma
 echo "0 9 * * * cd /home/wyy/clawcloud-autologin/ && NODE_OPTIONS=\"--no-deprecation\" /usr/bin/python3 /home/wyy/clawcloud-autologin/takeover_browser.py --sleep >> /home/wyy/clawcloud-autologin/run.log 2>&1" | crontab -u $SSH_USER -
 service cron start
 service nginx start
