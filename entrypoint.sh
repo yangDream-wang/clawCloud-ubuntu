18a0bf7728b3e8c6e0f65e6cf9135e007a520469
yangDream-wang <52024450+yangDream-wang@users.noreply.github.com>
2026-02-08 19:57:43 +0800

    更新cron 自动写入命令

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ usermod -aG sudo $SSH_USER
 echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
 echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
 
-echo "0 9 * * 1-5 cd /home/wyy/clawcloud-autologin/ && sleep \$(($(od -An -N2 -i /dev/urandom) % 28800)) && NODE_OPTIONS=\"--no-deprecation\" /usr/bin/python3 /home/wyy/clawcloud-autologin/takeover_browser.py  >> /home/wyy/clawcloud-autologin/run.log 2>&1" | crontab -u $SSH_USER -
+echo "0 9 * * * cd /home/wyy/clawcloud-autologin/ && NODE_OPTIONS=\"--no-deprecation\" /usr/bin/python3 /home/wyy/clawcloud-autologin/takeover_browser.py --sleep >> /home/wyy/clawcloud-autologin/run.log 2>&1" | crontab -u $SSH_USER -
 service cron start
+service nginx start
 if [ -n "$START_CMD" ]; then
     set -- $START_CMD
 fi
